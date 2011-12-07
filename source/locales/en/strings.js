93bcd5e03ef9abb9d4d2b7595d6b0b3a81d00908
Gabriele Coletta <gdmg92@gmail.com>
2011-12-07 19:43:01 +0100

    new option page with new translation system

---
+++ source/locales/en/strings.js
@@ -0,0 +1,17 @@
+var strings = {
+	"Bars color" : "Bars color",
+	"color of the bars" : "color of the bars",
+	"Border color" : "Border color",
+	"bar's border color" : "bar's border color",
+	"Size" : "Size",
+	"the size of the bars" : "the size of the bars",
+	"Solve slow scrolling" : "Solve slow scrolling",
+	"use this option if you are experiencing slow scrolling" : "use this option if you are experiencing slow scrolling",
+	"Auto hide" : "Auto hide",
+	"auto hide the scrollbars after a few seconds" : "auto hide the scrollbars after a few seconds",
+	"View bar only when mouse is over it" : "View bar only when mouse is over it",
+	"version" : "version",
+	"default value": "default value",
+	"General options:" : "General options:",
+	"Bar apparence:" : "Bar apparence:"
+};
---
+++ source/locales/nb/strings.js
@@ -0,0 +1,17 @@
+var strings = {
+	"Bars color" : "Feltfarge:",
+	"color of the bars" : "fargen på feltet",
+	"Border color" : "Rammefarge",
+	"bar's border color" : "fargen på feltets ramme",
+	"Size" : "Feltstørrelse:",
+	"the size of the bars" : "",
+	"Solve slow scrolling" : "Korriger treg rulling",
+	"use this option if you are experiencing slow scrolling" : "bruk dette valget om du opplever treg rulling",
+	"Auto hide" : "Skjul automatisk",
+	"auto hide the scrollbars after a few seconds" : "skjul rullefeltene etter noen sekunder",
+	"View bar only when mouse is over it" : "Kun vis rullefeltet når musepekeren er over sjakten",
+	"version" : "version",
+	"default value": "tilbakestill til forvalgt verdi",
+	"General options:" : "Generelle instillinger:",
+	"Bar apparence:" : "Rullefelt utseende:"
+};
